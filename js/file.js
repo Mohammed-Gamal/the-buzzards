fee9b1e3f912b6c893e07c039358b1ad00eb8b72
Mohamed Gamal <1Mohamed.Gamal54@gmail.com>
2022-08-05 15:30:25 +0200

    Update file.js

--- js/file.js
+++ js/file.js
@@ -13,27 +13,27 @@ async function renderBlocks() {
 }
 
 // handle disabling inspect element
-// document.body.oncontextmenu = () => {
-//   return false;
-// };
+document.body.oncontextmenu = () => {
+  return false;
+};
 
-// document.onkeydown = function (e) {
-//   if (event.keyCode == 123) {
-//     return false;
-//   }
-//   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-//     return false;
-//   }
-//   if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
-//     return false;
-//   }
-//   if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-//     return false;
-//   }
-//   if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-//     return false;
-//   }
-// };
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
 
 // handle scroll progress bar
 let scrollProgressSpan = document.querySelector("span.scroll-progress"),
